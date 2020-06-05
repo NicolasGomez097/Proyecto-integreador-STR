ab92b4a734e7b15c4150d2f7d91feaf1489cbac6
Gomez Nicolas <ngomez057@alumnos.iua.edu.ar>
2020-06-04 23:17:03 -0300

    Se arreglo la funcionalidad del socket con el select

--- cpp/pidServer.cpp
+++ cpp/pidServer.cpp
@@ -14,30 +14,34 @@ int main() {
     ServerTCP server;
 
     struct timeval timeout = {1, 0};
-    tick_fd = rtc_init(16);
+    tick_fd = rtc_init(1);
 
     // inicializacion socket TCP
     server.initSocketServer();
+    
+    printf("Server FD: %d\n", server.getFD());
+    fflush(stdout);
 
-    // Inicializando el SET para el select
-    FD_ZERO(&readfds);
-    FD_SET(tick_fd, &readfds);
-    FD_SET(server.getFD(), &readfds);
+    
 
     while (true)
     {
-        res = select(3, &readfds, NULL, NULL, &timeout);
+        // Inicializando el SET para el select
+        FD_ZERO(&readfds);
+        FD_SET(tick_fd, &readfds);
+        FD_SET(server.getFD(), &readfds);
+
+        res = select(5, &readfds, NULL, NULL, NULL);
  
-        if(FD_ISSET(tick_fd, &readfds) == 0) {
-            /*printf("*");
-            fflush(stdout);*/
+        if(FD_ISSET(tick_fd, &readfds)) {
+            printf("*");
+            fflush(stdout);
             rtc_tick();
         }
         if (FD_ISSET(server.getFD(), &readfds)) {
-            printf("HHHHHHHHHHHHH\n");
-            fflush(stdout);
             char* msg = server.readSocket();
-            printf("%s", msg);
+            server.sendMsg("Response");
+            printf("%s\n", msg);
             fflush(stdout);
         }
     }
